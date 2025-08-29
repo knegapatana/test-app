6fd4d3087a25038ffeeb821c307e97ccbe9d769a
knegapatana <knegapatana@micron.com>
2025-08-29 15:01:56 +0800

    Update streamlit_practice.py

--- streamlit_practice.py
+++ streamlit_practice.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("hi bb!")
+st.header("hi bb!")
 st.subheader(
     "i just started so this page doesn't have much and is pretty lame")
 st.write("u good?")
@@ -18,3 +18,4 @@ yes_button = st.button("yes", on_click=clicked_yes)
 no_button = st.button("no", on_click=clicked_no)
 
 st.image("bishan.png", caption="some bishan amk park scenery to motivate you")
+
