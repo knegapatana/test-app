342ccece17d6b691ed215ff95ec4f07ee9d5b96c
knegapatana <knegapatana@micron.com>
2025-08-29 14:47:14 +0800

    Add files via upload

---
+++ streamlit_practice.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.title("hi bb!")
+st.subheader(
+    "i just started so this page doesn't have much and is pretty lame")
+st.write("u good?")
+
+
+def clicked_yes():
+    st.write("yay! ♡ almost the end of the week liao, last push bb")
+
+
+def clicked_no():
+    st.write("jy 我的爱 ♡ you are smart af and you got this okay")
+
+
+yes_button = st.button("yes", on_click=clicked_yes)
+no_button = st.button("no", on_click=clicked_no)
+
+st.image("bishan.png", caption="some bishan amk park scenery to motivate you")
